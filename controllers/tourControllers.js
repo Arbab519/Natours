9b1a8adfab5c9216d014095aca3d155969e6bdf3
Arbab519 <bobetts786@gmail.com>
2023-04-18 16:05:27 +0500

    controllers

---
+++ controllers/tourControllers.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+
+// !Read Toursimple.json
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`,'utf-8'));
+
+
+//! HANDLERS
+exports.getAllTours= (req,res)=>{  
+    const result= res.status(200).json({
+        status :"success",
+        result : tours.length,
+        data: {
+            tours: tours
+        }
+    });
+};
+
+exports.getTour=  (req,res)=>{
+    //url variables store in req.params in object form
+    const id = req.params.id;
+
+
+    // find object from tours where id = url_id
+    const tour = tours.find(ele => ele.id == id);
+
+
+    // check if invalid id  or check if tour found or not
+    // if(id > tours.length-1){
+    if(! tour){
+        res.status(404).json({
+            status:'fail',
+            message:"Invalid ID"
+        });
+    }
+    res.status(200).json({
+        status :"success",
+        data: {
+            tour: tour
+        }
+    })
+};
+
+
+exports.updateTour= (req,res)=>{
+    //url variables store in req.params in object form
+    const id = req.params.id;
+ if(id > tours.length-1){
+        res.status(404).json({
+            status:'fail',
+            message:"Invalid ID"
+        });
+    }
+    res.status(200).json({
+        status :"success",
+        data: {
+            tour: "<data updated>"
+        }
+    })
+};
+
+
+exports.deleteTour = (req,res)=>{
+    //url variables store in req.params in object form
+    const id = req.params.id;
+ if(id > tours.length-1){
+        res.status(404).json({
+            status:'fail',
+            message:"Invalid ID"
+        });
+    }
+    res.status(204).json({})
+};
+
+exports.createTour =(req,res)=>{
+    //req is in req.body
+    //  console.log(req.body);
+    //creating new id by adding 1 to last object id 
+     const newId = tours[tours.length - 2].id++;
+     //creat new tour  giving it a new id manully  object.assign(to combile in one object)
+     const newTour = Object.assign({ id : newId },req.body);
+
+     // adding in existing tours above 
+     tours.push(newTour);
+
+     //Updating the tour-simple.json
+    //We have to JSON.sringify() the new tour to add in tours-simple.json
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err=>{
+    //  returning the response 
+    res.status(201).json({
+        status: "success",
+        data: {
+        tour: newTour
+        }
+    }) ;
+    });
+};
